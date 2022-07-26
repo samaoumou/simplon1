9ea4406acc20bb5f885313e99cec568c35588459
issa <i.ndiaye@isepdiamniadio.edu.sn>
2022-07-26 10:29:13 +0000

    first commit

---
+++ papa.c
@@ -0,0 +1,55 @@
+#include <stdio.h> 
+#include <string.h>
+#include <stdlib.h>
+int verif(char s[50]){                                                //création d'une fonction qui permet de compter le nombre de chiffre present
+int count=0;
+for (int i=0; i<strlen(s);i++)
+{
+    if(s[i]>='0'&& s[i]<='9')
+    {
+        count++;    
+    }
+}
+if (strlen(s)==count)
+{
+    return 1;
+}
+return 0;
+}
+int main(){
+    char code[50];
+    int i=0;
+    printf("entrer ton code: ");
+    scanf("%s",&code);
+    while (verif(code)==0)
+    {
+        printf("entrer entrer des nombres: ");
+         scanf("%s",&code);
+    }
+    int Tab[3] = {111,222,333};
+    int a=3;  
+    while (atoi(code)!=Tab[i])
+    {     
+        if (i<3)
+        {     
+            i++;
+        }
+        else 
+            {     
+                if(a>1)
+                    {         
+                        printf("incorrecte il vous reste %d tentative: ",a);         
+                        scanf("%s",&code);         
+                        i=0;         
+                        a--;     
+                    }
+                else 
+                   { 
+                        printf("fin de tentative");     
+                        return 0;     
+                   }      
+            } 
+    }    
+    printf("correcte");
+return 0;    
+}
--- ptr.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main(){
-    int tab[]={1,2,3};
-    int *p;
-    p=&tab[3];
-    printf("%p",p);
-    return 0;
-}
